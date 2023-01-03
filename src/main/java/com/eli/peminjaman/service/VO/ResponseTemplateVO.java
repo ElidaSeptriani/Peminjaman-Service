3ee8a177a2c7593a1a51edef1c953adeea623245
ElidaSeptriani <113493736+ElidaSeptriani@users.noreply.github.com>
2023-01-03 19:43:34 +0700

    Add files via upload

---
+++ src/main/java/com/eli/peminjaman/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eli.peminjaman.service.VO;
+
+import com.eli.peminjaman.service.entity.Peminjaman;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ResponseTemplateVO {
+
+    public void setPeminjaman(Peminjaman peminjaman) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setAnggota(Anggota anggota) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
